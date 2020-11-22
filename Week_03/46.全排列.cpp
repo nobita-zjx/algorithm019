5a0b56371af577f91d033e3250809b0d499e00c8
nobita <zheng_junxiong@163.com>
2020-11-23 03:43:59 +0800

    week03

---
+++ Week_03/46.全排列.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=cpp
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& res, vector<int>& output, int first, int len){
+        if (first == len) {
+            res.emplace_back(output);
+            return;
+        }
+        for (int i = first; i < len; ++i) {
+            swap(output[i], output[first]);
+            backtrack(res, output, first + 1, len);
+            swap(output[i], output[first]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> > res;
+        backtrack(res, nums, 0, (int)nums.size());
+        return res;
+    }
+};
+// @lc code=end
+
